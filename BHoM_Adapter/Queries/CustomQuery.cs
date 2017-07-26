5a46cdf1cd2a5c5c80cefb40a4c75a50de452aa5
adecler <arnaud.declercq@burohappold.com>
2017-07-26 14:21:31 +0100

    Initial definitions for Bson, Json, Cipher and Zip + adpater interface & queries

---
+++ BHoM_Adapter/Queries/CustomQuery.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.Adapter.Queries
+{
+    public class CustomQuery : IQuery
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+        public string Query { get; set; }
+
+
+        /***************************************************/
+        /**** Constructors                              ****/
+        /***************************************************/
+
+        public CustomQuery(string query = "")
+        {
+            Query = query;
+        }
+    }
+}
