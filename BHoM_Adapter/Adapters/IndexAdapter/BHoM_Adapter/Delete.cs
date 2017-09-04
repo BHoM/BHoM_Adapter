16be802b7482d1de77b83f95adf38f357cdae967
adecler <arnaud.declercq@burohappold.com>
2017-09-04 16:32:56 +0100

    First sweep of cleaning/clarifying the code

---
+++ BHoM_Adapter/Adapters/IndexAdapter/BHoM_Adapter/Delete.cs
@@ -0,0 +1,40 @@
+﻿using BH.oM.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.Adapter
+{
+    public abstract partial class IndexAdapter
+    {
+        /***************************************************/
+        /**** BHoM Adapter Methods                      ****/
+        /***************************************************/
+
+        protected override int Delete(Type type, string tag = "")
+        {
+            if (tag == "")
+            {
+                return Delete(type, null);
+            }
+            else
+            {
+                // Get all with tag
+                IEnumerable<BHoMObject> withTag = Read(type, tag);
+
+                // Get indices of all with that tag only
+                List<int> indices = withTag.Where(x => x.Tags.Count == 1).Select(x => (int)x.CustomData[AdapterId]).OrderBy(x => x).ToList();
+                Delete(type, indices);
+
+                // Remove tag if other tags as well
+                IEnumerable<BHoMObject> multiTags = withTag.Where(x => x.Tags.Count > 1);
+                UpdateTags(multiTags);
+
+                return indices.Count;
+            }
+
+        }
+    }
+}
---
+++ BHoM_Adapter/Adapters/IndexAdapter/BHoM_Adapter/Read.cs
@@ -0,0 +1,28 @@
+﻿using BH.oM.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.Adapter
+{
+    public abstract partial class IndexAdapter
+    {
+        /***************************************************/
+        /**** BHoM Adapter Methods                      ****/
+        /***************************************************/
+
+        protected override IEnumerable<BHoMObject> Read(Type type, string tag = "")
+        {
+            // Get the objects based on the indices
+            IEnumerable<BHoMObject> fromIndices = Read(type, null);
+
+            // Filter by tag if any 
+            if (tag == "")
+                return fromIndices;
+            else
+                return fromIndices.Where(x => x.Tags.Contains(tag));
+        }
+    }
+}
