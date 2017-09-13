73e80349ffc36a7db17c1b348d6a136acf147b94
adecler <arnaud.declercq@burohappold.com>
2017-09-13 17:11:38 +0100

    updates on the file adapter

--- File_Adapter/BHoMAdapter/Create.cs
+++ File_Adapter/BHoMAdapter/Create.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using BH.oM.Base;
+using MongoDB.Bson.IO;
+using MongoDB.Bson.Serialization;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +14,50 @@ namespace BH.Adapter.FileAdapter
     {
         protected override bool Create(IEnumerable<object> objects)
         {
-            throw new NotImplementedException();  //TODO: Implement Create method for the FileAdapter
+            IEnumerable<BHoMObject> objectsToPush = objects.Cast<BHoMObject>();
+
+            if (m_Readable)
+                return CreateJson(objectsToPush);
+            else
+                return CreateBson(objectsToPush);
+        }
+
+        private bool CreateBson(IEnumerable<BHoMObject> objects, bool clearFile = false)
+        {
+            try
+            {
+                FileStream stream = new FileStream(m_FilePath, clearFile ? FileMode.Create : FileMode.Append);
+                var writer = new BsonBinaryWriter(stream);
+                BsonSerializer.Serialize(writer, typeof(object), objects);
+                stream.Flush();
+                stream.Close();
+            }
+            catch (Exception e)
+            {
+                ErrorLog.Add(e.Message);
+                return false;
+            }
+
+            return true;
+        }
+
+
+        private bool CreateJson(IEnumerable<BHoMObject> objects, bool clearFile = false)
+        {
+            try
+            {
+                if (clearFile)
+                    File.WriteAllLines(m_FilePath, objects.Select(x => x.ToJson()));
+                else
+                    File.AppendAllLines(m_FilePath, objects.Select(x => x.ToJson()));
+            }
+            catch (Exception e)
+            {
+                ErrorLog.Add(e.Message);
+                return false;
+            }
+
+            return true;
         }
     }
 }
