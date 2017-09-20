a7f25e5dd922e09df3db7f5897ee65c79a0c90c9
Arnaud Declercq <arnaud.declercq@burohappold.com>
2017-09-20 16:36:02 +0100

    Replace method refactored

--- BHoM_Adapter/BHoMAdapter/CRUD/Replace.cs
+++ BHoM_Adapter/BHoMAdapter/CRUD/Replace.cs
@@ -35,41 +35,36 @@ namespace BH.Adapter
 
             //Read all the existing objects of that type
             IEnumerable<T> existing = Read(typeof(T)).Cast<T>();
+
+            // Replace objects that overlap and define the objects that still have to be pushed
             IEnumerable<T> objectsToCreate = newObjects;
+            bool overwriteObjects = false;
 
             if (PushConfiguration.ProcessInMemory)
             {
-                // Separate objects based on tags
-                List<T> multiTaggedObjects = existing.Where(x => x.Tags.Contains(tag) && x.Tags.Count > 1).ToList();
-                IEnumerable<T> nonTaggedObjects = existing.Where(x => !x.Tags.Contains(tag));
-
-                // Remove the tag from the multi-tags objects
-                multiTaggedObjects.ForEach(x => x.Tags.Remove(tag));
-
-                // Generate the list of objects to push
-                objectsToCreate = multiTaggedObjects.Concat(nonTaggedObjects).Concat(newObjects);
-
-                // Create objects
-                if (!Create(objectsToCreate, true))
-                    return false;
+                objectsToCreate = ReplaceInMemory(newObjects, existing, tag);
+                overwriteObjects = true;
             }
             else
             {
-                // Delete objects that have the tag
-                IEnumerable<T> objectsToDelete = existing.Where(x => x.Tags.Contains(tag) && x.Tags.Count == 1);
-                Delete(typeof(T), objectsToDelete.Select(x => x.CustomData[AdapterId]));
-
-                //
-
-                // Create objects
-                if (!Create(objectsToCreate))
-                    return false;
+                objectsToCreate = ReplaceThroughAPI(newObjects, existing, tag);
             }
 
-            //Make sure every material is tagged with id
-            IEqualityComparer<T> comparer = GetComparer<T>();
-            foreach (T item in objectsToPush)
-                item.CustomData[AdapterId] = objectsToCreate.First(x => comparer.Equals(x, item)).CustomData[AdapterId].ToString();
+            // Assign Id if needed
+            if (PushConfiguration.UseAdapterId)
+            {
+                AssignId(objectsToCreate);
+
+                // Map Ids to the original set of objects (before we extracted the distincts elements from it)
+                IEqualityComparer<T> comparer = GetComparer<T>();
+                foreach (T item in objectsToPush)
+                    item.CustomData[AdapterId] = objectsToCreate.First(x => comparer.Equals(x, item)).CustomData[AdapterId].ToString();
+            }
+
+
+            // Create objects
+            if (!Create(objectsToCreate, overwriteObjects))
+                return false;
 
             return true;
         }
@@ -93,62 +88,65 @@ namespace BH.Adapter
 
 
         /***************************************************/
-
-        public bool Replace<T>(IEnumerable<T> objectsToPush, string tag, Action<object> customDataWriter = null) where T: BHoMObject
-        {
-            List<T> objectList = objectsToPush.ToList();
-
-            // Make sure objects being pushed are tagged
-            if (tag != "")
-                objectList.ForEach(x => x.Tags.Add(tag));
-
-            // Delete objects that have the tag
-            Delete(typeof(T), tag);
-
-            // Add custom data to the objects to write
-            if (customDataWriter != null)
-                objectList.ForEach(x => customDataWriter(x));
-
-            // Finally Create the objects
-            return Create(objectsToPush);
-        }
-
+        /**** Private  Methods                          ****/
         /***************************************************/
 
-        public bool Replace(List<BHoMObject> objectsToPush, string tag = "", Action<object> customDataWriter = null)
+        protected void AssignId<T>(IEnumerable<T> objects) where T: BHoMObject
         {
-            // Make sure objects being pushed are tagged
-            if (tag != "")
-                objectsToPush.ForEach(x => x.Tags.Add(tag));
-
-            // Add custom data to the objects to write
-            if (customDataWriter != null)
-                objectsToPush.ForEach(x => customDataWriter(x));
-
-            // Finally Create the objects
-            return Create(objectsToPush, true);
-        }
-
-        /***************************************************/
-
-        protected bool Replace<T>(List<T> objectsToPush, IEqualityComparer<T> comparer, List<Type> dependencies, string tag = "", bool applyMerge = true) where T : BHoMObject
-        {
-            // Assure Objects to be pushed are distincts and have tags 
-            List<T> objectsToCreate = objectsToPush.Distinct(comparer).ToList();
-            objectsToCreate.ForEach(x => x.Tags.Add(tag));
-
-            // Get existing objects
-            IEnumerable<T> existingObjects = Pull(new FilterQuery(typeof(T))).Cast<T>();
-
-            // Merge and push the dependencies
-            foreach (Type t in dependencies)
+            bool refresh = true;
+            foreach (T item in objects)
             {
-                IEnumerable<object> merged = objectsToCreate.MergePropertyObjects<T>(t);
-                foreach (var typeGroup in merged.GroupBy(x => x.GetType()))
-                    if (!Replace(typeGroup as dynamic, tag))
-                        return false;
+                if (!item.CustomData.ContainsKey(AdapterId))
+                {
+                    item.CustomData[AdapterId] = GetNextId(typeof(T), refresh);
+                    refresh = false;
+                }
+            }
+        }
+
+        /***************************************************/
+
+        protected bool MergeIntoSet<T>(IEnumerable<T> objects, IEnumerable<T> set, out IEnumerable<Tuple<T,T>> mergedObjects, out IEnumerable<T> unmergedObjects) where T : BHoMObject
+        {
+            IEqualityComparer<T> comparer = GetComparer<T>();
+
+            VennDiagram<T> diagram = objects.CreateVennDiagram(set, comparer);
+            diagram.Intersection.ForEach(x => x.Item1.MapSpecialProperties(x.Item2, AdapterId));
+
+            mergedObjects = diagram.Intersection;
+            unmergedObjects = diagram.OnlySet1;
+
+            return true;
+        }
+
+        /***************************************************/
+
+        protected IEnumerable<T> ReplaceInMemory<T>(IEnumerable<T> newObjects, IEnumerable<T> existingOjects, string tag) where T : BHoMObject
+        {
+            // Separate objects based on tags
+            List<T> multiTaggedObjects = existingOjects.Where(x => x.Tags.Contains(tag) && x.Tags.Count > 1).ToList();
+            IEnumerable<T> nonTaggedObjects = existingOjects.Where(x => !x.Tags.Contains(tag));
+
+            // Remove the tag from the multi-tags objects
+            multiTaggedObjects.ForEach(x => x.Tags.Remove(tag));
+
+            // Merge objects if required
+            if (PushConfiguration.MergeWithComparer)
+            {
+                VennDiagram<T> diagram = newObjects.CreateVennDiagram(multiTaggedObjects.Concat(nonTaggedObjects), GetComparer<T>());
+                diagram.Intersection.ForEach(x => x.Item1.MapSpecialProperties(x.Item2, AdapterId));
+                newObjects = diagram.OnlySet1;
             }
 
+            // Generate the list of objects to push
+            IEnumerable<T> objectsToCreate = multiTaggedObjects.Concat(nonTaggedObjects).Concat(newObjects);
+            return objectsToCreate;
+        }
+
+        /***************************************************/
+
+        protected IEnumerable<T> ReplaceThroughAPI<T>(IEnumerable<T> newObjects, IEnumerable<T> existingObjects, string tag) where T : BHoMObject
+        {
             //Check if objects contains tag
             List<T> taggedObjects = existingObjects.Where(x => x.Tags.Contains(tag)).ToList();
             List<T> nonTaggedObjects = existingObjects.Where(x => !x.Tags.Contains(tag)).ToList();
@@ -157,66 +155,106 @@ namespace BH.Adapter
             foreach (T item in taggedObjects)
                 item.Tags.Remove(tag);
 
+            // Delete object without a tag left
+            Delete(typeof(T), taggedObjects.Where(x => x.Tags.Count == 0).Select(x => x.CustomData[AdapterId]));
 
-            Delete(taggedObjects.Where(x => x.Tags.Count == 0));
+            // Get objects without the tag that can potentially be merged with the new objects
+            IEqualityComparer<T> comparer = GetComparer<T>();
+            VennDiagram<T> diagram1 = newObjects.CreateVennDiagram(nonTaggedObjects, comparer);
 
+            // Check and map properties
+            diagram1.Intersection.ForEach(x => x.Item1.MapSpecialProperties(x.Item2, AdapterId));
 
-            if (applyMerge)
-            {
-                // Get objects without the tag that can potentially be merged with the new objects
-                VennDiagram<T> diagram1 = objectsToCreate.CreateVennDiagram(nonTaggedObjects, comparer);
+            // Get objectsmultiple tags that can potentially be merged with the new objects
+            VennDiagram<T> diagram2 = diagram1.OnlySet1.CreateVennDiagram(taggedObjects.Where(x => x.Tags.Count > 0), comparer);
 
-                // Check and map properties
-                diagram1.Intersection.ForEach(x => x.Item1.MapSpecialProperties(x.Item2, AdapterId));
+            // Check and map properties
+            diagram2.Intersection.ForEach(x => x.Item1.MapSpecialProperties(x.Item2, AdapterId));
 
-                // Get objectsmultiple tags that can potentially be merged with the new objects
-                VennDiagram<T> diagram2 = diagram1.OnlySet1.CreateVennDiagram(taggedObjects.Where(x => x.Tags.Count > 0), comparer);
+            //Update the tags
+            UpdateProperty(typeof(T), diagram2.OnlySet2.Select(x => x.CustomData[AdapterId]).ToList(), "Tags", diagram2.OnlySet2.Select(x => x.Tags));
 
-                // Check and map properties
-                diagram2.Intersection.ForEach(x => x.Item1.MapSpecialProperties(x.Item2, AdapterId));
+            //Delete objects to be replaced
+            Delete(diagram1.Intersection.Select(x => x.Item2).Concat(diagram2.Intersection.Select(x => x.Item2)));
 
-                //Update the tags
-                UpdateProperty(typeof(T), diagram2.OnlySet2.Select(x => x.CustomData[AdapterId]).ToList(), "Tags", diagram2.OnlySet2.Select(x => x.Tags));
-
-                //Preprocess unaffected objects
-                Create_PreProcess(diagram2.OnlySet1);
-
-                //Delete objects to be replaced
-                Delete(diagram1.Intersection.Select(x => x.Item2).Concat(diagram2.Intersection.Select(x => x.Item2)));
-
-            }
-            else
-            {
-                //Preprocess objects to create
-                Create_PreProcess(objectsToCreate);
-            }
-
-            //Create objects
-            if (Create(objectsToCreate))
-            {
-                //Make sure every material is tagged with id
-                foreach (T item in objectsToPush)
-                    item.CustomData[AdapterId] = objectsToCreate.First(x => comparer.Equals(x, item)).CustomData[AdapterId].ToString();
-                return true;
-            }
-
-            return false;
-        }
-
-
-        /***************************************************/
-        /**** Private  Methods                          ****/
-        /***************************************************/
-
-        private void Create_PreProcess<T>(IEnumerable<T> objects) where T: BHoMObject
-        {
-            bool refresh = true;
-            foreach (T item in objects)
-            {
-                item.CustomData[AdapterId] = GetNextId(typeof(T), refresh);
-                refresh = false;
-            }
+            // return the objects to push
+            return newObjects;
         }
 
     }
 }
+
+
+// Legacy replace method
+
+//protected bool Replace<T>(List<T> objectsToPush, IEqualityComparer<T> comparer, List<Type> dependencies, string tag = "", bool applyMerge = true) where T : BHoMObject
+//{
+//    // Assure Objects to be pushed are distincts and have tags 
+//    List<T> objectsToCreate = objectsToPush.Distinct(comparer).ToList();
+//    objectsToCreate.ForEach(x => x.Tags.Add(tag));
+
+//    // Get existing objects
+//    IEnumerable<T> existingObjects = Pull(new FilterQuery(typeof(T))).Cast<T>();
+
+//    // Merge and push the dependencies
+//    foreach (Type t in dependencies)
+//    {
+//        IEnumerable<object> merged = objectsToCreate.MergePropertyObjects<T>(t);
+//        foreach (var typeGroup in merged.GroupBy(x => x.GetType()))
+//            if (!Replace(typeGroup as dynamic, tag))
+//                return false;
+//    }
+
+//    //Check if objects contains tag
+//    List<T> taggedObjects = existingObjects.Where(x => x.Tags.Contains(tag)).ToList();
+//    List<T> nonTaggedObjects = existingObjects.Where(x => !x.Tags.Contains(tag)).ToList();
+
+//    //Remove tag from existing objects
+//    foreach (T item in taggedObjects)
+//        item.Tags.Remove(tag);
+
+
+//    Delete(taggedObjects.Where(x => x.Tags.Count == 0));
+
+
+//    if (applyMerge)
+//    {
+//        // Get objects without the tag that can potentially be merged with the new objects
+//        VennDiagram<T> diagram1 = objectsToCreate.CreateVennDiagram(nonTaggedObjects, comparer);
+
+//        // Check and map properties
+//        diagram1.Intersection.ForEach(x => x.Item1.MapSpecialProperties(x.Item2, AdapterId));
+
+//        // Get objectsmultiple tags that can potentially be merged with the new objects
+//        VennDiagram<T> diagram2 = diagram1.OnlySet1.CreateVennDiagram(taggedObjects.Where(x => x.Tags.Count > 0), comparer);
+
+//        // Check and map properties
+//        diagram2.Intersection.ForEach(x => x.Item1.MapSpecialProperties(x.Item2, AdapterId));
+
+//        //Update the tags
+//        UpdateProperty(typeof(T), diagram2.OnlySet2.Select(x => x.CustomData[AdapterId]).ToList(), "Tags", diagram2.OnlySet2.Select(x => x.Tags));
+
+//        //Preprocess unaffected objects
+//        AssignId(diagram2.OnlySet1);
+
+//        //Delete objects to be replaced
+//        Delete(diagram1.Intersection.Select(x => x.Item2).Concat(diagram2.Intersection.Select(x => x.Item2)));
+
+//    }
+//    else
+//    {
+//        //Preprocess objects to create
+//        AssignId(objectsToCreate);
+//    }
+
+//    //Create objects
+//    if (Create(objectsToCreate))
+//    {
+//        //Make sure every material is tagged with id
+//        foreach (T item in objectsToPush)
+//            item.CustomData[AdapterId] = objectsToCreate.First(x => comparer.Equals(x, item)).CustomData[AdapterId].ToString();
+//        return true;
+//    }
+
+//    return false;
+//}
