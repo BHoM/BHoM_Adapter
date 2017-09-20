860c6737367bc247be3e455e1bc222a2f8759225
Arnaud Declercq <arnaud.declercq@burohappold.com>
2017-09-20 16:36:02 +0100

    Replace method refactored

--- BHoM_Adapter/BHoMAdapter/Configs/PushConfig.cs
+++ BHoM_Adapter/BHoMAdapter/Configs/PushConfig.cs
@@ -11,6 +11,7 @@ namespace BH.Adapter
         public bool ProcessInMemory { get; set; } = true;
         public bool SeparateProperties { get; set; } = false;
         public bool MergeWithComparer { get; set; } = false;
+        public bool UseAdapterId { get; set; } = true;
     }
 }
 
