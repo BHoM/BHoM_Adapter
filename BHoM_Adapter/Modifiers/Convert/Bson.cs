1e1e4fabccbadb9c37ded49a03a7f7f0e01ab8d3
adecler <arnaud.declercq@burohappold.com>
2017-10-23 16:02:00 +0800

    Handling CustomObject in Bson Conversion

--- BHoM_Adapter/Modifiers/Convert/Bson.cs
+++ BHoM_Adapter/Modifiers/Convert/Bson.cs
@@ -1,7 +1,9 @@
-﻿using MongoDB.Bson;
+﻿using BH.oM.Base;
+using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,6 +21,16 @@ namespace BH.Adapter
                 BsonDocument.TryParse(obj as string, out document);
                 return document;
             }
+            else if (obj is CustomObject)
+            {
+                CustomObject co = obj as CustomObject;
+                BsonDocument doc = co.CustomData.ToBsonDocument();
+                if (co.Name.Length > 0)
+                    doc["Name"] = co.Name;
+                if (co.Tags.Count > 0)
+                    doc["Tags"] = new BsonArray(co.Tags);
+                return doc;
+            }
             else
                 return obj.ToBsonDocument();
         }
@@ -27,15 +39,30 @@ namespace BH.Adapter
 
         public static object FromBson(BsonDocument bson)
         {
-            /*Type type = BH.Engine.Reflection.Create.Type(bson.GetValue("_t").AsString);
-            if (!BsonClassMap.IsClassMapRegistered(type))
-                BsonClassMap.RegisterClassMap(new BsonClassMap(type));*/
-
             if (!m_TypesRegistered)
                 RegisterTypes();
 
             bson.Remove("_id");
-            return BsonSerializer.Deserialize(bson, typeof(object));
+            object obj = BsonSerializer.Deserialize(bson, typeof(object));
+            if (obj is ExpandoObject)
+            {
+                Dictionary<string, object> dic = new Dictionary<string, object>(obj as ExpandoObject);
+                CustomObject co = new CustomObject();
+                if (dic.ContainsKey("Name"))
+                {
+                    co.Name = dic["Name"] as string;
+                    dic.Remove("Name");
+                }   
+                if (dic.ContainsKey("Tags"))
+                {
+                    co.Tags = new HashSet<string>(((List<object>)dic["Tags"]).Cast<string>());
+                    dic.Remove("Tags");
+                }
+                co.CustomData = dic;
+                return co;
+            }
+            else
+                return obj;
         }
 
         /*******************************************/
@@ -45,11 +72,7 @@ namespace BH.Adapter
             foreach (Type type in BH.Engine.Reflection.Create.TypeList())
             {
                 if (!type.IsGenericType)
-                {
                     BsonClassMap.LookupClassMap(type);
-                    /*if (!BsonClassMap.IsClassMapRegistered(type))
-                        BsonClassMap.RegisterClassMap(new BsonClassMap(type));*/
-                }
             }
 
             m_TypesRegistered = true;
