7e49eea6696badb3f7042c8b56c40181fa7f3865
adecler <arnaud.declercq@burohappold.com>
2017-10-23 13:30:04 +0800

    Adding BatchQuery object

---
+++ BHoM_Adapter/Queries/BatchQuery.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+using BH.oM.Base;
+
+namespace BH.Adapter.Queries
+{
+    public class BatchQuery : IQuery
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+        public List<IQuery> Queries { get; set; } = new List<IQuery>();
+
+
+        /***************************************************/
+        /**** Constructors                              ****/
+        /***************************************************/
+
+        public BatchQuery() { }
+
+        /***************************************************/
+
+        public BatchQuery(IEnumerable<IQuery> queries)
+        {
+            Queries = queries.ToList();
+        }
+
+        
+    }
+}
