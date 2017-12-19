0968cbceba54e927106af3d88769ea6f80118491
adecler <arnaud.declercq@burohappold.com>
2017-12-19 17:28:06 +0800

    fixing stuff for serialization, even for Chrome

---
+++ BHoM_Adapter/Modifiers/Convert/BsonSerializers/ColourSerializer.cs
@@ -0,0 +1,59 @@
+﻿using MongoDB.Bson.IO;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.Adapter.Modifiers.Convert.BsonSerializers
+{
+    public class ColourSerializer : SerializerBase<System.Drawing.Color>
+    {
+        public override void Serialize(BsonSerializationContext context, BsonSerializationArgs args, System.Drawing.Color value)
+        {
+            context.Writer.WriteStartDocument();
+
+            context.Writer.WriteName("A");
+            context.Writer.WriteInt32(value.A);
+
+            context.Writer.WriteName("R");
+            context.Writer.WriteInt32(value.R);
+
+            context.Writer.WriteName("G");
+            context.Writer.WriteInt32(value.G);
+
+            context.Writer.WriteName("B");
+            context.Writer.WriteInt32(value.B);
+
+            context.Writer.WriteEndDocument();
+        }
+
+        /*******************************************/
+
+        public override Color Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
+        {
+            INameDecoder decoder = Utf8NameDecoder.Instance;
+
+            context.Reader.ReadStartDocument();
+
+            context.Reader.ReadName(decoder);
+            int a = context.Reader.ReadInt32();
+
+            context.Reader.ReadName(decoder);
+            int r = context.Reader.ReadInt32();
+
+            context.Reader.ReadName(decoder);
+            int g = context.Reader.ReadInt32();
+
+            context.Reader.ReadName(decoder);
+            int b = context.Reader.ReadInt32();
+
+            context.Reader.ReadEndDocument();
+
+            return Color.FromArgb(a, r, g, b);
+        }
+    }
+}
---
+++ BHoM_Adapter/Modifiers/Convert/BsonSerializers/CustomObjectSerializer.cs
@@ -0,0 +1,47 @@
+﻿using BH.oM.Base;
+using MongoDB.Bson.IO;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.Adapter.Modifiers.Convert.BsonSerializers
+{
+    public class CustomObjectSerializer : SerializerBase<CustomObject>
+    {
+        public override void Serialize(BsonSerializationContext context, BsonSerializationArgs args, CustomObject value)
+        {
+            Dictionary<string, object> data = new Dictionary<string, object>(value.CustomData);
+            data["BHoM_Guid"] = value.BHoM_Guid;
+
+            context.Writer.WriteStartDocument();
+
+            if (value.Tags.Count > 0)
+            {
+                context.Writer.WriteName("Name");
+                BsonSerializer.Serialize(context.Writer, value.Name);
+            }
+
+            if (value.Name.Length > 0)
+            {
+                context.Writer.WriteName("Tags");
+                context.Writer.WriteStartArray();
+                foreach (string tag in value.Tags)
+                    context.Writer.WriteString(tag);
+                context.Writer.WriteEndArray();
+            }
+
+            foreach (KeyValuePair<string, object> kvp in data)
+            {
+                context.Writer.WriteName(kvp.Key);
+                BsonSerializer.Serialize(context.Writer, kvp.Value);
+            }
+            context.Writer.WriteEndDocument();
+        }
+
+    }
+}
