5c95ff871780771709489e5484c3d9cba210e4fd
INaslund <isak.naslund@burohappold.com>
2018-02-02 14:52:38 +0100

    Fixes for the replace method

--- BHoM_Adapter/AdapterConfig.cs
+++ BHoM_Adapter/AdapterConfig.cs
@@ -6,6 +6,7 @@
         public bool SeparateProperties { get; set; } = false;
         public bool MergeWithComparer { get; set; } = false;
         public bool UseAdapterId { get; set; } = true;
+        public bool CloneBeforePush { get; set; } = true;
     }
 }
 
