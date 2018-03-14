8d3dcc55359fbf890be881a1c6b1d3bf1c38d92c
IsakNaslund <isak.naslund@burohappold.com>
2018-03-14 17:42:59 +0100

    Create file and folder if they do not exist

---
+++ File_Adapter/CRUD/Push.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.Base;
+
+namespace BH.Adapter.FileAdapter
+{
+    public partial class FileAdapter
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public override List<IObject> Push(IEnumerable<IObject> objects, string tag = "", Dictionary<string, object> config = null)
+        {
+            CreateFileAndFolder();
+
+            return base.Push(objects, tag, config);
+        }
+    }
+}
