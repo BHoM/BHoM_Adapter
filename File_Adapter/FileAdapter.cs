9626ea3bd56cbf63372ac16f5f57bacb623a1d61
IsakNaslund <isak.naslund@burohappold.com>
2018-03-14 17:42:59 +0100

    Create file and folder if they do not exist

--- File_Adapter/FileAdapter.cs
+++ File_Adapter/FileAdapter.cs
@@ -1,4 +1,8 @@
-﻿namespace BH.Adapter.FileAdapter
+﻿using System.Linq;
+using System;
+using System.IO;
+
+namespace BH.Adapter.FileAdapter
 {
     public partial class FileAdapter : BHoMAdapter
     {
@@ -13,14 +17,41 @@
 
         public FileAdapter(string folder = "C:\\", string fileName = "objects", bool readable = false)
         {
+
+            if (folder.Count() > 2 && folder.ElementAt(1) != ':')
+            {
+                folder = @"C:\Users\" + Environment.UserName + @"\AppData\Roaming\BHoM\DataSets\" + folder;
+            }
+
             if (!folder.EndsWith("\\"))
                 folder += "\\";
 
             m_FilePath = folder + fileName + (readable ? ".json" : ".bson");
+
+
+
             m_Readable = readable;
             this.Config.UseAdapterId = false;
         }
 
+        /***************************************************/
+        /**** Private Methods                           ****/
+        /***************************************************/
+
+        private void CreateFileAndFolder()
+        {
+            string directoryPath = Path.GetDirectoryName(m_FilePath);
+            if (!Directory.Exists(directoryPath))
+                Directory.CreateDirectory(directoryPath);
+
+
+            if (!File.Exists(m_FilePath))
+            {
+                FileStream stream = File.Create(m_FilePath);
+                stream.Dispose();
+                stream.Close();
+            }
+        }
 
         /***************************************************/
         /**** Private Fields                            ****/
