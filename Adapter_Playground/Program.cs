fd2d09fc043accf49fd3c92cd1e74b0cc4081a58
adecler <arnaud.declercq@burohappold.com>
2018-04-14 14:33:36 +0800

    _Test -> Playground

---
+++ Adapter_Playground/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using BH.oM.Structural.Elements;
+using BH.oM.Geometry;
+using MongoDB.Bson;
+using MongoDB.Bson.IO;
+using System;
+
+namespace Adapter_Playground
+{
+    class Program
+    {
+        /***************************************************/
+        /**** Entry Point                               ****/
+        /***************************************************/
+
+        static void Main(string[] args)
+        {
+            Console.Read();
+        }
+
+
+        /***************************************************/
+        /**** Private Methods                           ****/
+        /***************************************************/
+
+
+
+        /***************************************************/
+    }
+}
