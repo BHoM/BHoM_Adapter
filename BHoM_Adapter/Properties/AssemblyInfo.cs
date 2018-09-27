06f5b7ee6cc0eff90a8f280149864c4f8bc710d6
Andrew Wakeman <andrew.wakeman@burohappold.com>
2018-09-27 10:18:43 +0100

    Create nuget package build specifications and dependencies on BHoM libraries
    With fallback mechanism for instances where there is no nuget source supplying the libraries
    
    Issue #57

--- BHoM_Adapter/Properties/AssemblyInfo.cs
+++ BHoM_Adapter/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BHoM_Adapter")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("BH.Adapter")]
+[assembly: AssemblyDescription("BHoM Adapter")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BHoM_Adapter")]
+[assembly: AssemblyCompany("BuroHappold")]
+[assembly: AssemblyProduct("BH.Adapter")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
