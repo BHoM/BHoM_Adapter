cfdf43d556f12e663f0ec5f1fb9f22fb2a2bf9e3
Alessio Lombardi <alessio.lombardi@burohappold.com>
2019-10-08 15:19:45 +0100

    Moved Push and Pull method in the body of FileAdapter.cs, out of the CRUD folder.

--- File_Adapter/FileAdapter.cs
+++ File_Adapter/FileAdapter.cs
@@ -24,20 +24,20 @@ using System.Linq;
 using System;
 using System.IO;
 using BH.oM.Reflection.Attributes;
+using System.Collections.Generic;
+using BH.oM.Base;
+using BH.oM.Data.Requests;
+using BH.Engine.Base;
 
 namespace BH.Adapter.FileAdapter
 {
     public partial class FileAdapter : BHoMAdapter
     {
         /***************************************************/
-        /**** Properties                                ****/
-        /***************************************************/
-
-        /***************************************************/
-        /**** Constructors                              ****/
+        /**** Constructor                               ****/
         /***************************************************/
         [Input("folder", "Defaults to the path of your default drive (usually C://)")]
-        [Input("fileName","Insert filename with extension.\nCurrently supports only .json and .bson file types.")]
+        [Input("fileName", "Insert filename with extension.\nCurrently supports only .json and .bson file types.")]
         public FileAdapter(string folder = null, string fileName = "")
         {
             if (folder == null)
@@ -57,10 +57,57 @@ namespace BH.Adapter.FileAdapter
             string ext = Path.GetExtension(m_FilePath);
 
             if (ext != ".json" && ext != ".bson")
-                    Engine.Reflection.Compute.RecordError($"File_Adapter currently supports only .json and .bson extension types.\nSpecified file extension: {ext}");
+                Engine.Reflection.Compute.RecordError($"File_Adapter currently supports only .json and .bson extension types.\nSpecified file extension: {ext}");
 
             m_isJSON = ext == ".json";
             this.Config.UseAdapterId = false;
+        }
+
+        /***************************************************/
+        /**** Public Adapter Methods overrides          ****/
+        /***************************************************/
+
+        public override List<IObject> Push(IEnumerable<IObject> objects, string tag = "", Dictionary<string, object> config = null)
+        {
+            if (!Path.HasExtension(m_FilePath))
+            {
+                Engine.Reflection.Compute.RecordError($"Please include the extension type in the FileName input.");
+                return null;
+            }
+
+            CreateFileAndFolder();
+
+            // Wrap non-BHoM objects into a Custom BHoMObject to make them work as BHoMObjects. 
+            List<IObject> objectsToPush = Modify.WrapNonBHoMObjects(objects, Config, tag, config).ToList();
+
+            // Clone the objects for immutability in the UI. CloneBeforePush should always be true, except for very specific cases.
+            objectsToPush = Config.CloneBeforePush ? objectsToPush.Select(x => x.DeepClone()).ToList() : objects.ToList();
+
+            IEnumerable<IBHoMObject> bhomObjects = objectsToPush.Where(x => x is IBHoMObject).Cast<IBHoMObject>();
+
+            if (bhomObjects.Count() != objects.Count())
+                Engine.Reflection.Compute.RecordWarning("The file adapter can currently only be used with BHoMObjects." + Environment.NewLine +
+                    "If you want to push non-BHoMobject, specify a push config with the option `WrapNonBHoMObject` set to true.");
+
+            bool success = this.Replace<IBHoMObject>(bhomObjects, tag);
+
+            return success ? objectsToPush : new List<IObject>();
+        }
+
+        public override IEnumerable<object> Pull(IRequest request, Dictionary<string, object> config = null)
+        {
+            if (!System.IO.File.Exists(m_FilePath))
+            {
+                Engine.Reflection.Compute.RecordError($"File not found: {m_FilePath} - Cannot pull from this file");
+                return null;
+            }
+            else if (!Path.HasExtension(m_FilePath))
+            {
+                Engine.Reflection.Compute.RecordError($"Please include the extension type in the FileName input.");
+                return null;
+            }
+
+            return base.Pull(request, config);
         }
 
         /***************************************************/
