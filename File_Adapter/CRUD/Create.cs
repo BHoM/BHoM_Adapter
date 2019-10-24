b16b64feb99159db4a7c8b8c4db7e791d294c706
Alessio Lombardi <alessio.lombardi@burohappold.com>
2019-10-24 16:59:12 +0100

    #113 remove ErrorLog Adapter property

--- File_Adapter/CRUD/Create.cs
+++ File_Adapter/CRUD/Create.cs
@@ -65,7 +65,7 @@ namespace BH.Adapter.FileAdapter
             }
             catch (Exception e)
             {
-                ErrorLog.Add(e.Message);
+                BH.Engine.Reflection.Compute.RecordError(e.Message);
                 return false;
             }
 
