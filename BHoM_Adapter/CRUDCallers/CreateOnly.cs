66e5286d2e7cf84415cfa2dbeff46ceccf3a8de3
Alessio Lombardi <alessio.lombardi@burohappold.com>
2019-11-11 12:45:41 +0000

    New folder structure: `CRUDCallers` and `Extra`. New methods: `CreateOnly`, `DependenciesCreateOnly`, `DependenciesUpdateOnly`

---
+++ BHoM_Adapter/CRUDCallers/CreateOnly.cs
@@ -0,0 +1,83 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2018, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.Engine.Reflection;
+using BH.oM.Base;
+using BH.oM.Data.Collections;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+
+namespace BH.Adapter
+{
+    public abstract partial class BHoMAdapter
+    {
+        /***************************************************/
+        /**** CRUDCallers Methods                       ****/
+        /***************************************************/
+        // These methods call the CRUD methods as appropriate.
+
+        [Description("Performs the only the Create for the specified objects and, if Config.HandleDependencies is true, their dependencies.")]
+        protected virtual bool CreateOnly<T>(IEnumerable<T> objectsToPush, string tag = "") where T : IBHoMObject
+        {
+            List<T> newObjects = objectsToPush.ToList();
+
+            // Make sure objects  are tagged
+            if (tag != "")
+                newObjects.ForEach(x => x.Tags.Add(tag));
+
+            // Merge and push the dependencies
+            if (Config.HandleDependencies)
+            {
+                var dependencyObjects = GetDependencyObjects<T>(newObjects, tag); //first-level dependencies
+
+                foreach (var depObj in dependencyObjects)
+                    if (!DependenciesCreateOnly(depObj.Value as dynamic, tag))
+                        return false;
+            }
+
+            return Create(newObjects);
+        }
+
+        protected virtual bool DependenciesCreateOnly<T>(IEnumerable<T> objectsToCreate, string tag = "") where T : IBHoMObject
+        {
+            // Make sure objects are distinct 
+            List<T> objects = objectsToCreate.Distinct(Comparer<T>()).ToList();
+
+            // Make sure objects are tagged
+            if (tag != "")
+                objects.ForEach(x => x.Tags.Add(tag));
+
+            // Create any sub-dependency 
+            var dependencyObjects = GetDependencyObjects<T>(objectsToCreate, tag);
+            foreach (var depObj in dependencyObjects)
+                DependenciesCreateOnly(depObj.Value as dynamic);
+
+            return Create(objects);
+        }
+
+
+    }
+}
