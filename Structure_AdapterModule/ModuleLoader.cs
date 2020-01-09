bdf793d1b41dbe2bedb40a575b7729ca1854cc1d
Alessio Lombardi <alessio.lombardi@burohappold.com>
2020-01-09 10:48:05 +0000

    Added description to ModuleLoader

--- Structure_AdapterModule/ModuleLoader.cs
+++ Structure_AdapterModule/ModuleLoader.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace BH.Adapter.Modules
 {
     public static class ModuleLoader
     {
+        [Description("Invoke this method in any Toolkit Adapter constructor to load functionality from the Structural Adapter Module.")]
         public static void LoadStructuralModules(this BHoMAdapter adapter)
         {
             adapter.AdapterModules.Add(new CopyNodeProperties());
