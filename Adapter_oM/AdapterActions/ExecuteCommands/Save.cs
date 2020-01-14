bbce7109b9b79235f2a1e5c727e9f29f59028dc6
Alessio Lombardi <alessio.lombardi@burohappold.com>
2020-01-14 17:34:36 +0000

    Added IExecuteCommand interface and few related commands.

--- Adapter_oM/AdapterActions/ExecuteCommands/Save.cs
+++ Adapter_oM/AdapterActions/ExecuteCommands/Save.cs
@@ -1,6 +1,6 @@
-/*
+﻿/*
  * This file is part of the Buildings and Habitats object Model (BHoM)
- * Copyright (c) 2015 - 2018, the respective contributors. All rights reserved.
+ * Copyright (c) 2015 - 2019, the respective contributors. All rights reserved.
  *
  * Each contributor holds copyright over their respective contributions.
  * The project versioning (Git) records all such contribution source information.
@@ -20,32 +20,17 @@
  * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
  */
 
-using BH.oM.Base;
-using BH.Engine.Base;
-using BH.oM.Data.Requests;
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
 using System.ComponentModel;
-using BH.oM.Adapter;
-using BH.oM.Reflection;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace BH.Adapter
+namespace BH.oM.Adapter.Commands
 {
-    public abstract partial class BHoMAdapter
+    [Description("Should be used to instruct the external software to save a file or model.")]
+    public class Save : IExecuteCommand
     {
-        /******************************************************/
-        /**** Public Adapter Methods "Adapter ACTIONS"    *****/
-        /******************************************************/
-        /* These methods represent Actions that the Adapter can complete. 
-           They are publicly available in the UI as individual components, e.g. in Grasshopper, under BHoM/Adapters tab. */
-
-        [Description("Sends specific commands to the external software, if supported by its API. Has to be implemented (overridden) in the specific Toolkit.")]
-        public virtual Output<object, bool> Execute(IExecuteCommand command, ActionConfig actionConfig = null)
-        {
-            return new Output<object, bool>() { Item1 = null, Item2 = false};
-        }
     }
 }
