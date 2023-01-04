84e713a02cf5867d0f2dc86557bef513387d7164
Alessio Lombardi <alessio.lombardi@burohappold.com>
2023-01-04 17:37:38 +0000

    Update GetDependencySortedObjects.cs

--- Adapter_Engine/Query/GetDependencySortedObjects.cs
+++ Adapter_Engine/Query/GetDependencySortedObjects.cs
@@ -183,9 +183,6 @@ namespace BH.Engine.Adapter
                         orderedObjects.RemoveAt(j);
                         orderedObjects.Insert(i, kv2);
                     }
-
-                    if (found)
-                        break;
                 }
             }
 
