a6be52c1486509f3c4195c0c89cc37b7577f8bf4
Isak Naslund <Isak.Naslund@burohappold.com>
2023-01-09 14:16:51 +0100

    Add specific module for gravity loads

---
+++ Structure_AdapterModules/GetGravityLoadElementsWithoutID.cs
@@ -0,0 +1,74 @@
+/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2023, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.oM.Base;
+using BH.oM.Adapter;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Reflection;
+using System.ComponentModel;
+using BH.oM.Structure.Elements;
+using System.Collections;
+using BH.oM.Geometry;
+using BH.oM.Structure.Constraints;
+using BH.oM.Structure.Loads;
+
+
+namespace BH.Adapter.Modules
+{
+    [Description("Get all elements of type T that do not contain an adapter ID of the expected type from the provided GravityLoads. Avoids the need to again read and check against elements already in the model.")]
+    public class GetGravityLoadElementsWithoutID<T> : IGetDependencyModule<GravityLoad, T> where T : IBHoMObject
+    {
+        /***************************************************/
+        /**** Interface method                          ****/
+        /***************************************************/
+
+        public IEnumerable<T> GetDependencies(IEnumerable<GravityLoad> objects)
+        {
+            List<T> noIdLoadObjects = new List<T>();
+            foreach (GravityLoad load in objects)
+            {
+                if(load?.Objects?.Elements != null)
+                    noIdLoadObjects.AddRange(load.Objects.Elements.OfType<T>().Where(x => x != null && !x.Fragments.Contains(m_adapterIdType)));
+            }
+            return noIdLoadObjects;
+        }
+
+        /***************************************************/
+        /**** Constructors                              ****/
+        /***************************************************/
+
+        public GetGravityLoadElementsWithoutID(IBHoMAdapter adapter)
+        {
+            m_adapterIdType = adapter.AdapterIdFragmentType;
+        }
+
+        /***************************************************/
+
+        private Type m_adapterIdType;
+
+        /***************************************************/
+    }
+}
+
+
