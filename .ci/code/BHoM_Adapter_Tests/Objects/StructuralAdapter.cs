ddd2557906e53b6da6e7e35c530dca3618f0a298
Isak Näslund <isak.naslund@burohappold.com>
2023-01-20 09:03:02 +0100

    Apply suggestions from code review
    Co-authored-by: Alessio Lombardi <alessio.lombardi@burohappold.com>

--- .ci/code/BHoM_Adapter_Tests/Objects/StructuralAdapter.cs
+++ .ci/code/BHoM_Adapter_Tests/Objects/StructuralAdapter.cs
@@ -124,12 +124,12 @@ namespace BH.Tests.Adapter
 
             List<IBHoMObject> modelObjects = Created.Where(x => x.Item1.IsAssignableFrom(type)).SelectMany(x => x.Item2).ToList();
 
-            MethodInfo method = typeof(Engine.Adapter.Query).GetMethod("GetDependencyTypes");
+            MethodInfo method = typeof(Engine.Adapter.Query).GetMethod(nameof(Engine.Adapter.Query.GetDependencyTypes));
             method = method.MakeGenericMethod(type);
 
             List<Type> dependencyTypes = method.Invoke(null, new object[] { this }) as List<Type>;
 
-            MethodInfo readCached = typeof(BHoMAdapter).GetMethods(BindingFlags.NonPublic | BindingFlags.Instance).FirstOrDefault(x => x.Name == "GetCachedOrRead");
+            MethodInfo readCached = typeof(BHoMAdapter).GetMethods(BindingFlags.NonPublic | BindingFlags.Instance).FirstOrDefault(x => x.Name == nameof(GetCachedOrRead));
 
             foreach (Type t in dependencyTypes)
             {
