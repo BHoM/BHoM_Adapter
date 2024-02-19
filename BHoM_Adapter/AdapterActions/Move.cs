b0a13e330845e330e6d7a2fab9f0d172b1cf4580
Albin Bergh <albin.bergh@burohappold.com>
2024-02-19 09:57:28 +0000

    Added move actions

--- BHoM_Adapter/AdapterActions/Move.cs
+++ BHoM_Adapter/AdapterActions/Move.cs
@@ -56,6 +56,26 @@ namespace BH.Adapter
 
             return to.Push(objects.Cast<IObject>(), tag, pushType, pushConfig).Count() == count;
         }
+
+        /******************************************************/
+
+        [Description("Performs an action prior to any move actions. For example can be used to open up a file for repeated move actions.")]
+        public virtual bool BeforeMove()
+        {
+            m_HasRunPreMoveAction = true;
+            return true;
+        }
+
+        /******************************************************/
+
+        [Description("Performs an action after any move actions. For example can be used to close a file for repeated move actions.")]
+        public virtual bool AfterMove()
+        {
+            m_HasRunPreMoveAction = false; //Reset for next move action
+            return true;
+        }
+
+        /******************************************************/
     }
 }
 
